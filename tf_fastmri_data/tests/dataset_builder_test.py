e05fa82737516697ee7df4d77d59118417a36dd2
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-08-05 18:14:54 +0200

    added path for dataset builder too

--- tf_fastmri_data/tests/dataset_builder_test.py
+++ tf_fastmri_data/tests/dataset_builder_test.py
@@ -3,11 +3,28 @@ import pytest
 from tf_fastmri_data.dataset_builder import FastMRIDatasetBuilder
 
 
+key_map = {
+    # multicoil
+    True: {
+        'train': 'fastmri_tmp_multicoil_train',
+        'val': 'fastmri_tmp_multicoil_val',
+        'test': 'fastmri_tmp_multicoil_test',
+    },
+    # not multicoil
+    False: {
+        'train': 'fastmri_tmp_singlecoil_train',
+        'val': 'fastmri_tmp_singlecoil_val',
+        'test': 'fastmri_tmp_singlecoil_test',
+    },
+}
+
 @pytest.mark.parametrize('dataset', ['train', 'test', 'val'])
 @pytest.mark.parametrize('multicoil', [True, False])
-def test_dataset_builder_init(dataset, multicoil):
-    FastMRIDatasetBuilder(dataset=dataset, multicoil=multicoil, prebuild=False)
+def test_dataset_builder_init(create_full_fastmri_test_tmp_dataset, dataset, multicoil):
+    path = create_full_fastmri_test_tmp_dataset[key_map[multicoil][dataset]]
+    FastMRIDatasetBuilder(path=path, dataset=dataset, multicoil=multicoil, prebuild=False)
 
-def test_dataset_builder_error():
+def test_dataset_builder_error(create_full_fastmri_test_tmp_dataset):
+    path = create_full_fastmri_test_tmp_dataset['fastmri_tmp_singlecoil_train']
     with pytest.raises(NotImplementedError):
-        FastMRIDatasetBuilder(prebuild=True)
+        FastMRIDatasetBuilder(path=path, prebuild=True)
