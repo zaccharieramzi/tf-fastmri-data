657e962e9832e2635cc62881e081d49ebf6e8c20
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-09-14 16:15:43 +0200

    updated corresponding unit test

--- tf_fastmri_data/tests/datasets/noisy_test.py
+++ tf_fastmri_data/tests/datasets/noisy_test.py
@@ -11,13 +11,18 @@ file_contrast = 'CORPD_FBK'
 @pytest.mark.parametrize('slice_random', [True, False])
 @pytest.mark.parametrize('noise_input', [True, False])
 @pytest.mark.parametrize('noise_power', [30, (0, 50)])
-def test_cartesian_dataset_train(create_full_fastmri_test_tmp_dataset, contrast, slice_random, noise_input, noise_power):
-    path = create_full_fastmri_test_tmp_dataset['fastmri_tmp_singlecoil_train']
-    ds = NoisyFastMRIDatasetBuilder(
-        path=path,
-        contrast=contrast,
-        slice_random=slice_random,
-    )
-    (image_noisy, *_others), model_outputs = next(ds.preprocessed_ds.as_numpy_iterator())
-    np.testing.assert_equal(model_outputs.shape[-3:], [320, 320, 1])
-    np.testing.assert_equal(image_noisy.shape[-3:], [320, 320, 1])
+@pytest.mark.parametrize('noise_mode', ['uniform', 'gaussian'])
+def test_cartesian_dataset_train(create_full_fastmri_test_tmp_dataset, contrast, slice_random, noise_input, noise_power, noise_mode):
+    if not (noise_mode == 'gaussian' and isinstance(noise_power, tuple)):
+        path = create_full_fastmri_test_tmp_dataset['fastmri_tmp_singlecoil_train']
+        ds = NoisyFastMRIDatasetBuilder(
+            path=path,
+            contrast=contrast,
+            slice_random=slice_random,
+            noise_input=noise_input,
+            noise_power=noise_power,
+            noise_mode=noise_mode,
+        )
+        (image_noisy, *_others), model_outputs = next(ds.preprocessed_ds.as_numpy_iterator())
+        np.testing.assert_equal(model_outputs.shape[-3:], [320, 320, 1])
+        np.testing.assert_equal(image_noisy.shape[-3:], [320, 320, 1])
