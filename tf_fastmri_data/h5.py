fff9fb11b4313d5eea0a15458041b595b8b96611
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-09-14 13:56:13 +0200

    added the possibility to only use the image in the h5 loading

--- tf_fastmri_data/h5.py
+++ tf_fastmri_data/h5.py
@@ -4,9 +4,12 @@ import h5py
 import ismrmrd
 
 
-def load_data_from_file(filename, slice_random=False):
+def load_data_from_file(filename, slice_random=False, no_kspace=False):
     with h5py.File(filename, 'r') as h5_obj:
-        kspace = h5_obj['kspace']
+        if no_kspace:
+            kspace = None
+        else:
+            kspace = h5_obj['kspace']
         if 'reconstruction_esc' in h5_obj.keys():
             image = h5_obj['reconstruction_esc']
         elif 'reconstruction_rss' in h5_obj.keys():
@@ -16,7 +19,8 @@ def load_data_from_file(filename, slice_random=False):
         if slice_random:
             kspace, image = _slice_selection(kspace, image)
         else:
-            kspace = kspace[()]
+            if kspace is not None:
+                kspace = kspace[()]
             if image is not None:
                 image = image[()]
         mask = h5_obj.get('mask', None)
@@ -31,7 +35,8 @@ def load_data_from_file(filename, slice_random=False):
 def _slice_selection(kspace, image):
     i_max = kspace.shape[0] - 1
     i_slice = random.randint(0, i_max)
-    kspace = kspace[i_slice:i_slice+1]
+    if kspace is not None:
+        kspace = kspace[i_slice:i_slice+1]
     if image is not None:
         image = image[i_slice:i_slice+1]
     return kspace, image
