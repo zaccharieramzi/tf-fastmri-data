211ecff375de8016097b59f6cf9eb6382e28b7af
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-09-15 18:08:00 +0200

    added some size adjustment utils

---
+++ tf_fastmri_data/preprocessing_utils/size_adjustment.py
@@ -0,0 +1,22 @@
+import tensorflow as tf
+
+def pad(x, size):
+    shape = tf.shape(x)[-1]
+    to_pad = size[-1] - shape
+    padded_x = tf.pad(
+        x,
+        [
+            (0, 0),
+            (0, 0),
+            (0, 0),
+            (to_pad//2, to_pad//2)
+        ]
+    )
+    return padded_x
+
+
+def crop(x, size):
+    shape = tf.shape(x)[-1]
+    to_crop = shape - size[-1]
+    cropped_x = x[..., to_crop//2:-to_crop//2]
+    return cropped_x
