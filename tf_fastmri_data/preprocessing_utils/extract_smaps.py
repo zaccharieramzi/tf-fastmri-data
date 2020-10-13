c16ec8ea850186e427f7da074f91f0ae4bda31f8
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-10-13 10:42:03 +0200

    made smaps extraction faster and with less code

--- tf_fastmri_data/preprocessing_utils/extract_smaps.py
+++ tf_fastmri_data/preprocessing_utils/extract_smaps.py
@@ -22,37 +22,18 @@ def extract_smaps(kspace, low_freq_percentage=8):
     Returns:
         tf.Tensor: extracted raw sensitivity maps.
     """
-    n_low_freq = tf.cast(tf.shape(kspace)[-2:] * low_freq_percentage / 100, tf.int32)
-    center_dimension = tf.cast(tf.shape(kspace)[-2:] / 2, tf.int32)
+    k_shape = tf.shape(kspace)[-2:]
+    n_low_freq = tf.cast(k_shape * low_freq_percentage / 100, tf.int32)
+    center_dimension = tf.cast(k_shape / 2, tf.int32)
     low_freq_lower_locations = center_dimension - tf.cast(n_low_freq / 2, tf.int32)
     low_freq_upper_locations = center_dimension + tf.cast(n_low_freq / 2, tf.int32)
-    ###
-    # NOTE: the following stands for in numpy:
-    # low_freq_mask = np.zeros_like(kspace)
-    # low_freq_mask[
-    #     ...,
-    #     low_freq_lower_locations[0]:low_freq_upper_locations[0],
-    #     low_freq_lower_locations[1]:low_freq_upper_locations[1]
-    # ] = 1
-    x_range = tf.range(low_freq_lower_locations[0], low_freq_upper_locations[0])
-    y_range = tf.range(low_freq_lower_locations[1], low_freq_upper_locations[1])
+    ### Masking strategy
+    x_range = tf.range(0, k_shape[0])
+    y_range = tf.range(0, k_shape[1])
     X_range, Y_range = tf.meshgrid(x_range, y_range)
-    X_range = tf.reshape(X_range, (-1,))
-    Y_range = tf.reshape(Y_range, (-1,))
-    low_freq_mask_indices = tf.stack([X_range, Y_range], axis=-1)
-    # we have to transpose because only the first dimension can be indexed in
-    # scatter_nd
-    scatter_nd_perm = [2, 3, 0, 1]
-    low_freq_mask = tf.scatter_nd(
-        indices=low_freq_mask_indices,
-        updates=tf.ones([
-            tf.size(X_range),
-            tf.shape(kspace)[0],
-            tf.shape(kspace)[1]],
-        ),
-        shape=[tf.shape(kspace)[i] for i in scatter_nd_perm],
-    )
-    low_freq_mask = tf.transpose(low_freq_mask, perm=scatter_nd_perm)
+    X_mask = tf.logical_and(X_range <= low_freq_upper_locations[0], X_range >= low_freq_lower_locations[0])
+    Y_mask = tf.logical_and(Y_range <= low_freq_upper_locations[1], Y_range >= low_freq_lower_locations[1])
+    low_freq_mask = tf.transpose(tf.logical_and(X_mask, Y_mask))[None, None, :]
     ###
     low_freq_kspace = kspace * tf.cast(low_freq_mask, kspace.dtype)
     shifted_kspace = ifftshift(low_freq_kspace, axes=[2, 3])
