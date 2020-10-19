ba72fa5bc5348c63dcd87fa9e5f7ddfd615cce6e
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2020-10-19 16:19:25 +0200

    generalised map fn trick to cartesian fourier transform

--- tf_fastmri_data/preprocessing_utils/fourier/cartesian.py
+++ tf_fastmri_data/preprocessing_utils/fourier/cartesian.py
@@ -1,3 +1,5 @@
+import multiprocessing
+
 import tensorflow as tf
 from tensorflow.python.ops.signal.fft_ops import ifft2d, ifftshift, fftshift
 
@@ -5,4 +7,24 @@ from tensorflow.python.ops.signal.fft_ops import ifft2d, ifftshift, fftshift
 def ortho_ifft2d(kspace):
     axes = [len(kspace.shape) - 2, len(kspace.shape) - 1]
     scaling_norm = tf.cast(tf.math.sqrt(tf.cast(tf.math.reduce_prod(tf.shape(kspace)[-2:]), 'float32')), kspace.dtype)
-    return scaling_norm * fftshift(ifft2d(ifftshift(kspace, axes=axes)), axes=axes)
+    if len(kspace.shape) == 4:
+        # multicoil case
+        ncoils = tf.shape(kspace)[1]
+    n_slices = tf.shape(kspace)[0]
+    k_shape_x = tf.shape(kspace)[-2]
+    k_shape_y = tf.shape(kspace)[-1]
+    shifted_kspace = ifftshift(kspace, axes=axes)
+    batched_shifted_kspace = tf.reshape(shifted_kspace, (-1, k_shape_x, k_shape_y))
+    batched_shifted_image = tf.map_fn(
+        ifft2d,
+        batched_shifted_kspace,
+        parallel_iterations=multiprocessing.cpu_count(),
+    )
+    if len(kspace.shape) == 4:
+        # multicoil case
+        image_shape = [n_slices, k_shape_x, k_shape_y]
+    else:
+        image_shape = [n_slices, ncoils, k_shape_x, k_shape_y]
+    shifted_image = tf.reshape(batched_shifted_image, image_shape)
+    image = fftshift(shifted_image, axes=axes)
+    return scaling_norm * image
