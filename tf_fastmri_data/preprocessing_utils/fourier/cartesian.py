81fac581070d1722b02f2bc36cbeea0426770666
chaithyagr <chaithyagr@gitlab.com>
2021-02-18 16:01:17 +0100

    Add fft2d

--- tf_fastmri_data/preprocessing_utils/fourier/cartesian.py
+++ tf_fastmri_data/preprocessing_utils/fourier/cartesian.py
@@ -1,7 +1,7 @@
 import multiprocessing
 
 import tensorflow as tf
-from tensorflow.python.ops.signal.fft_ops import ifft2d, ifftshift, fftshift
+from tensorflow.python.ops.signal.fft_ops import ifft2d, ifftshift, fftshift, fft2d
 
 
 def ortho_ifft2d(kspace):
@@ -30,3 +30,32 @@ def ortho_ifft2d(kspace):
     shifted_image = tf.reshape(batched_shifted_image, image_shape)
     image = fftshift(shifted_image, axes=axes)
     return scaling_norm * image
+
+
+def ortho_fft2d(image):
+    image = tf.cast(image, 'complex64')
+    axes = [len(image.shape) - 2, len(image.shape) - 1]
+    scaling_norm = tf.cast(tf.math.sqrt(tf.cast(tf.math.reduce_prod(tf.shape(image)[-2:]), 'float32')), image.dtype)
+    if len(image.shape) == 4:
+        # multicoil case
+        ncoils = tf.shape(kspace)[1]
+    n_slices = tf.shape(image)[0]
+    k_shape_x = tf.shape(image)[-2]
+    k_shape_y = tf.shape(image)[-1]
+    shifted_image = fftshift(image, axes=axes)
+    batched_shifted_image = tf.reshape(shifted_image, (-1, k_shape_x, k_shape_y))
+    batched_shifted_kspace = tf.map_fn(
+        fft2d,
+        batched_shifted_image,
+        parallel_iterations=multiprocessing.cpu_count(),
+    )
+    if len(image.shape) == 4:
+        # multicoil case
+        kspace_shape = [n_slices, ncoils, k_shape_x, k_shape_y]
+    elif len(image.shape) == 3:
+        kspace_shape = [n_slices, k_shape_x, k_shape_y]
+    else:
+        kspace_shape = [k_shape_x, k_shape_y]
+    shifted_kspace = tf.reshape(batched_shifted_kspace, kspace_shape)
+    kspace = fftshift(shifted_kspace, axes=axes)
+    return scaling_norm * kspace
