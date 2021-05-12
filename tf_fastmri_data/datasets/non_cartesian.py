9fa207730eaf331447f16553dec256d2f72e94f0
Chaithya G R <chaithyagr@gmail.com>
2021-05-12 09:56:51 +0200

    Add codes for generic case of trajectory (#26)
    Co-authored-by: Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
    Co-authored-by: chaithyagr <chaithyagr@gitlab.com>

--- tf_fastmri_data/datasets/non_cartesian.py
+++ tf_fastmri_data/datasets/non_cartesian.py
@@ -13,6 +13,7 @@ from tf_fastmri_data.preprocessing_utils.non_cartesian_trajectories import (
     get_debugging_cartesian_trajectory,
 )
 from tf_fastmri_data.preprocessing_utils.scaling import scale_tensors
+from tf_fastmri_data.preprocessing_utils.crop import adjust_image_size
 
 
 IMAGE_SIZE = (640, 400)
@@ -24,13 +25,17 @@ class NonCartesianFastMRIDatasetBuilder(FastMRIDatasetBuilder):
             acq_type='radial',
             dcomp=True,
             scale_factor=1e6,
+            traj=None,
+            crop_image_data=False,
             **kwargs,
         ):
         self.image_size = image_size
         self.acq_type = acq_type
+        self.traj = traj
         self._check_acq_type()
         self.dcomp = dcomp
         self.scale_factor = scale_factor
+        self.crop_image_data = crop_image_data
         self.nufft_obj = KbNufftModule(
             im_size=self.image_size,
             grid_size=None,
@@ -46,9 +51,13 @@ class NonCartesianFastMRIDatasetBuilder(FastMRIDatasetBuilder):
         self._check_dcomp_multicoil()
 
     def _check_acq_type(self,):
-        if self.acq_type not in ['spiral', 'radial', 'cartesian_debug']:
+        if self.acq_type not in ['spiral', 'radial', 'cartesian_debug', 'other']:
             raise ValueError(
                 f'acq_type must be spiral, radial or cartesian_debug but is {self.acq_type}'
+            )
+        if self.acq_type == 'other' and self.traj is None:
+            raise ValueError(
+                f'Please provide a trajectory as input in case `acq_type` is `other`'
             )
 
     def _check_mode(self,):
@@ -66,6 +75,8 @@ class NonCartesianFastMRIDatasetBuilder(FastMRIDatasetBuilder):
             traj = get_debugging_cartesian_trajectory()
         elif self.acq_type == 'spiral':
             traj = get_spiral_trajectory(self.image_size, af=self.af)
+        elif self.acq_type == 'other':
+            traj = self.traj
         return traj
 
     def preprocessing(self, image, kspace):
@@ -82,11 +93,16 @@ class NonCartesianFastMRIDatasetBuilder(FastMRIDatasetBuilder):
             )
         traj = tf.repeat(traj, tf.shape(image)[0], axis=0)
         orig_image_channels = ortho_ifft2d(kspace)
-        nc_kspace = nufft(self.nufft_obj, orig_image_channels, traj, self.image_size)
+        nc_kspace = nufft(self.nufft_obj, orig_image_channels, traj, self.image_size, multicoil=self.multicoil)
         nc_kspace, image = scale_tensors(nc_kspace, image, scale_factor=self.scale_factor)
         image = image[..., None]
         nc_kspaces_channeled = nc_kspace[..., None]
-        orig_shape = tf.ones([tf.shape(kspace)[0]], dtype=tf.int32) * tf.shape(kspace)[-1]
+        orig_shape = tf.ones([tf.shape(kspace)[0]], dtype=tf.int32) * self.image_size[-1]
+        if self.crop_image_data:
+            image = adjust_image_size(image, self.image_size)
+        else:
+            output_shape = tf.shape(image)[1:][None, :]
+            output_shape = tf.tile(output_shape, [tf.shape(image)[0], 1])
         extra_args = (orig_shape,)
         if self.dcomp:
             dcomp = tf.ones(
@@ -96,7 +112,9 @@ class NonCartesianFastMRIDatasetBuilder(FastMRIDatasetBuilder):
             extra_args += (dcomp,)
         model_inputs = (nc_kspaces_channeled, traj)
         if self.multicoil:
-            smaps = non_cartesian_extract_smaps(nc_kspace, traj, dcomp, nufftob_back, orig_shape)
+            smaps = non_cartesian_extract_smaps(nc_kspace, traj, dcomp, nufftob_back, self.image_size)
             model_inputs += (smaps,)
+        if not self.crop_image_data:
+            model_inputs += (output_shape,)
         model_inputs += (extra_args,)
         return model_inputs, image
--- tf_fastmri_data/preprocessing_utils/fourier/non_cartesian.py
+++ tf_fastmri_data/preprocessing_utils/fourier/non_cartesian.py
@@ -1,38 +1,17 @@
 import tensorflow as tf
 from tfkbnufft import kbnufft_forward
 
-
-def _pad_for_nufft(image, im_size):
-    shape = tf.shape(image)[-1]
-    to_pad = im_size[-1] - shape
-    padded_image = tf.pad(
-        image,
-        [
-            (0, 0),
-            (0, 0),
-            (0, 0),
-            (to_pad//2, to_pad//2)
-        ]
-    )
-    return padded_image
+from ..crop import adjust_image_size
 
 
-def _crop_for_nufft(image, im_size):
-    shape = tf.shape(image)[-1]
-    to_crop = shape - im_size[-1]
-    cropped_image = image[..., to_crop//2:-to_crop//2]
-    return cropped_image
-
-def nufft(nufft_ob, image, ktraj, image_size=None):
+def nufft(nufft_ob, image, ktraj, image_size=None, multicoil=True):
+    if image_size is not None:
+        image = adjust_image_size(
+            image,
+            image_size,
+            multicoil=multicoil,
+        )
     forward_op = kbnufft_forward(nufft_ob._extract_nufft_interpob(), multiprocessing=True)
     shape = tf.shape(image)[-1]
-    if image_size is not None:
-        image_adapted = tf.cond(
-            tf.math.greater(shape, image_size[-1]),
-            lambda: _crop_for_nufft(image, image_size),
-            lambda: _pad_for_nufft(image, image_size),
-        )
-    else:
-        image_adapted = image
-    kspace = forward_op(image_adapted, ktraj)
+    kspace = forward_op(image, ktraj)
     return kspace
