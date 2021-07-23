a76e771e2c160196fe1b14192b4b1c601cd6cd83
Zaccharie Ramzi <zaccharie.ramzi@gmail.com>
2021-07-23 17:41:14 +0200

    corrected init and setup to have a placeholder for version

---
+++ tf_fastmri_data/__init__.py
@@ -0,0 +1,8 @@
+"""Package info"""
+
+__version__ = "develop"
+__author__ = 'Zaccharie Ramzi'
+__author_email__ = 'zaccharie.ramzi@gmail.com'
+__license__ = 'MIT'
+__homepage__ = 'https://github.com/zaccharieramzi/tf-fastmri-data'
+__docs__ = 'Data pipelines for the fastMRI dataset in TensorFlow.'
